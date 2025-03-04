bead1c43bed2c75820c0ed24b637de6239c94d7f
mobren11 <mobren11@gmail.com>
2025-03-04 17:37:32 -0600

    explicitly handle POST only
    the logic should only be triggered on POST, just dump the file contents to the screen if it is GET

--- writer.php
+++ writer.php
@@ -5,21 +5,32 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// add some values from the server
-$_POST['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
-$_POST['REQUEST_TIME'] = $_SERVER['REQUEST_TIME'];
-
-// escape special chars
-foreach (array_keys($_POST) as $key)
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-    $_POST[$key] = htmlspecialchars($_POST[$key]);
+    // add some values from the server
+    $_POST['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    $_POST['REQUEST_TIME'] = $_SERVER['REQUEST_TIME'];
+
+    // escape special chars
+    foreach (array_keys($_POST) as $key)
+    {
+        $_POST[$key] = htmlspecialchars($_POST[$key]);
+    }
+
+    // convert to json and append
+    $out = json_encode($_POST) . "\n";
+    file_put_contents("./record.csv", $out, FILE_APPEND);
+
+
+    echo "Wrote the following to the record:<br>" . "<pre>" . $out . "</pre>";
+    exit;
 }
 
-// convert to json and append
-$out = json_encode($_POST) . "\n";
-file_put_contents("./record.csv", $out, FILE_APPEND);
+if ($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    $out = file_get_contents($_SERVER['SCRIPT_FILENAME']);
+    $out = htmlspecialchars($out);
+    echo "<pre>" . $out . "</pre>";
+}
 
-
-echo "Wrote the following to the record:<br>" . "<pre>" . $out . "</pre>";
-exit;
 ?>
