402be5d529d3611a65d9f57aca85bdd0f189e41e
mobren11 <mobren11@gmail.com>
2025-03-04 16:59:44 -0600

    improve writer by writing json
    rather than listing keys after values, we should just use JSON
    
    NOTE: because we are limited to append only (for security reasons), we cannot maintain proper JSON syntax. some manual recovery will be required for parsing.

--- writer.php
+++ writer.php
@@ -5,32 +5,21 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$_POST['TIMESTAMP_SELECTED'] = strtotime($_POST['TIMESTAMP_SELECTED']);
+// add some values from the server
+$_POST['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'];
+$_POST['REQUEST_TIME'] = $_SERVER['REQUEST_TIME'];
 
-$master = "";
-
-$master = $master . $_SERVER['REMOTE_ADDR'] . ", ";
-$master = $master . $_SERVER['REQUEST_TIME'] . ", "; 
-
-$keys = array_keys($_POST);
-foreach ($keys as $key)
+// escape special chars
+foreach (array_keys($_POST) as $key)
 {
-    $master = $master . $_POST[$key] . ", ";
-}
-$master = $master . ":::, ";
-
-$master = $master . 'REMOTE_ADDR' . ", ";
-$master = $master . 'REQUEST_TIME' . ", "; 
-
-foreach ($keys as $key)
-{
-    $master = $master . $key . ", ";
+    $_POST[$key] = htmlspecialchars($_POST[$key]);
 }
 
-file_put_contents("./record.csv", $master, FILE_APPEND);
+// convert to json and append
+$out = json_encode($_POST) . "\n";
+file_put_contents("./record.csv", $out, FILE_APPEND);
 
-echo "Wrote the following to the record: \n\n\n" . $master;
 
+echo "Wrote the following to the record:<br>" . "<pre>" . $out . "</pre>";
 exit;
-
 ?>
